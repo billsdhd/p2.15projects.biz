a38fc7142a2487af64bbcc55bfca4c9d5cebffff
Bill J <bill@bills-air.home>
2013-10-27 01:47:37 -0400

    Install the frame work

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="utf-8"/>
-  <title>Project 2</title>
+	<meta charset="utf-8"/>
+	<title>Project 2</title>
 </head>
 
 <body>
-<h3>Project 2</h3>
+	<h3>Project 2</h3>
 
 </body>
+
 </html>
