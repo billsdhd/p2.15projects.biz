85eb389002f66bb8b0ae4663163350ac9119bb68
Bill J <bill@bills-air.home>
2013-11-04 06:33:29 -0500

    Database name

--- config/config.php
+++ config/config.php
@@ -30,7 +30,7 @@ A email designated to receive messages from the server. Examples:
 */ 	
 	define('SYSTEM_EMAIL', 'billsdhd@gmail.com'); 
 
-# Default DB name for this app
+# Default DB name for this app 
 	define('DB_NAME', 'projec16_p2_15projects_biz'); 
 
 # Timezone
